6a41c9d92034164be61104509ac2f1abb40b41aa
dkenniff <dylankenniff@gmail.com>
2024-03-17 21:44:54 -0400

    Implemented Enemy AI - Added chase and wander states - implemented speed change - added lose condition and level reset

--- core/src/edu/cornell/gdiac/physics/AIController.java
+++ core/src/edu/cornell/gdiac/physics/AIController.java
@@ -1,5 +1,236 @@
 package edu.cornell.gdiac.physics;
 
+import com.badlogic.gdx.math.Vector2;
+import edu.cornell.gdiac.physics.enemy.Enemy;
+import edu.cornell.gdiac.physics.player.UrsaModel;
+
 public class AIController {
+
+    /**
+     * Enumeration to encode the FSM
+     */
+    private static enum FSMState {
+        /** The enemy just spawned in */
+        SPAWN,
+        /** The enemy is patrolling around without a target */
+        WANDER,
+        /** Enemy has seen/heard something but hasn't been triggered yet. */
+        CONFUSED,
+        /** Enemy knows you're there, preparing to chase/attack */
+        ALERT,
+        /** Enemy is chasing the player */
+        CHASE,
+        /** Enemy is attacking the player */
+        ATTACK,
+        /** Enemy has been stunned and can not move */
+        STUN
+    }
+
+    /**  */
+
+    // Constants for chase algorithms
+    /** Time in range before changing confused to attack */
+    private static final int CONFUSE_TIME = 20;
+    /** Time enemy will be stunned for */
+    private static final int STUN_TIME = 30;
+    /** Range enemy will stay engaged in if chasing but not in cone */
+    private static final int MEMORY_RANGE = 4;
+    /** Amount of uninterrupted time player can spend in cone before losing */
+    private static final int LIFE_TIME = 120;
+    /** Amount of time between wandering direction change */
+    private static final int DIR_TIME = 70;
+    /** Amount of time enemy can sprint */
+    private static final int MAX_STAMINA = 70;
+
+    // Instance Attributes
+    /** The enemy being controlled by this AIController */
+    private Enemy enemy;
+    /** The player being targeted */
+    private UrsaModel ursa;
+    /** The enemy's current state in the FSM */
+    private FSMState state;
+    /** The ship's next action (may include firing). */
+    private int move; // A ControlCode
+    /** Ticks spent confused */
+    private int ticks_confused = 0;
+    /** Ticks spent stunned */
+    private int ticks_stunned = 0;
+    /** Ticks spent in range of attack */
+    private int ticks_attacked = 0;
+    /** Amount of "sprint" enemy has */
+    private int curr_stamina = MAX_STAMINA;
+    /** Ticks since last wander dir change */
+    private int ticks_since_change = 0;
+    /** The number of ticks since we started this controller  */
+    private long ticks;
+    /** Determines whether this enemy has won and caught Ursa */
+    private boolean hasWon = false;
+
+    /**
+     * Creates an AIController for the ship with the given id.
+     */
+    public AIController(Enemy enemy,  UrsaModel ursa) {
+        this.enemy = enemy;
+        //this.board = board;
+        this.ursa = ursa;
+
+        state = FSMState.SPAWN;
+        //move  = CONTROL_NO_ACTION;
+        ticks = 0;
+
+    }
+
+    public void reset() {
+        hasWon = false;
+        state = FSMState.SPAWN;
+        ticks_attacked = 0;
+        ticks_confused = 0;
+        ticks_stunned = 0;
+        ticks_since_change = 0;
+        curr_stamina = MAX_STAMINA;
+    }
+
+
+    private void changeStateIfApplicable() {
+        switch(state) {
+            case SPAWN:
+                if (ticks > CONFUSE_TIME) state = FSMState.WANDER;
+
+                break;
+
+            case WANDER:
+                if (enemy.isAlerted()) {
+                    state = FSMState.CONFUSED;
+                }
+
+                break;
+
+            case CONFUSED:
+                if (enemy.isAlerted() && ticks_confused < CONFUSE_TIME) {
+                    state = FSMState.CONFUSED;
+                    ticks_confused++;
+                } else if (enemy.isAlerted()) {
+                    state = FSMState.CHASE;
+                    ticks_confused = 0;
+                } else {
+                    state = FSMState.WANDER;
+                    ticks_confused = 0;
+                }
+
+                break;
+            case ALERT:
+
+                break;
+            case CHASE:
+                if (ticks_attacked >= LIFE_TIME ) {
+                    state = FSMState.CHASE;
+                    hasWon = true;
+                }
+                if (enemy.isAlerted()) {
+                    state = FSMState.CHASE;
+                    ticks_attacked++;
+                } else {
+                    if (ticks_confused < CONFUSE_TIME) {
+                        ticks_confused++;
+                        ticks_attacked = 0;
+                        state = FSMState.CHASE;
+                    } else {
+                        ticks_confused = 0;
+                        state = FSMState.WANDER;
+                    }
+                }
+
+                break;
+
+            case ATTACK:
+
+                break;
+
+            case STUN:
+
+                break;
+
+        }
+    }
+
+    public void update() {
+        //enemy.applyForce();
+    }
+
+    public void getAction() {
+        ticks++;
+
+        changeStateIfApplicable();
+        System.out.println(state.toString());
+
+        switch (state) {
+            case SPAWN:
+                enemy.setVX(0);
+                enemy.setVY(0);
+                break;
+            case WANDER:
+
+                Vector2 action = new Vector2(0,0);
+
+                if (enemy.getX() < 5) {
+                    enemy.setMoveDirection(15,0);
+                } else if (enemy.getX() > 15) {
+                    enemy.setMoveDirection(-15,0);
+                }
+
+                if (enemy.getVX() < 0) {
+                    enemy.setLookDirection(-1,0);
+                } else {
+                    enemy.setLookDirection(1,0);
+                }
+
+//                enemy.setLookDirection(action.x, 0);
+//                enemy.setVX(action.x * 10);
+
+//                if ((enemy.getVX() != 0 || enemy.getVY() != 0) && ticks_since_change < DIR_TIME) {
+//                    ticks_since_change++;
+//                    return;
+//                }
+//                Vector2 action = new Vector2((float) (2 * Math.random() - 1),
+//                        (float) (2 * Math.random() - 1)).nor();
+//                enemy.setLookDirection(action.x, action.y);
+//                enemy.setVX(action.x);
+//                enemy.setVY(action.y);
+//                ticks_since_change = 0;
+                curr_stamina = Math.min(MAX_STAMINA, curr_stamina + 1);
+                return;
+
+            case CONFUSED:
+                System.out.println("Enemy is confused");
+                enemy.setVX(0);
+                enemy.setVY(0);
+                curr_stamina = Math.min(MAX_STAMINA, curr_stamina + 1);
+                break;
+            case CHASE:
+                action =  new Vector2(ursa.getX() - enemy.getX(), ursa.getY() - enemy.getY()).nor();
+
+                int multiplier = curr_stamina != 0 ? 4 : 2;
+                curr_stamina = Math.max(curr_stamina - 1, 0);
+
+                action.x *= multiplier;
+                action.y *= multiplier;
+                enemy.setVX(action.x);
+                enemy.setVY(action.y);
+                enemy.setLookDirection(action.x, action.y);
+
+                break;
+        }
+
+    }
+
+
+
+    public Enemy getEnemy() { return enemy; }
+
+    public boolean isWon() { return hasWon; }
+
+    public void setWon(boolean value) { hasWon = value; }
+
+    public void resetLifeTime() { ticks_attacked = 0; }
 
 }
--- core/src/edu/cornell/gdiac/physics/SceneModel.java
+++ core/src/edu/cornell/gdiac/physics/SceneModel.java
@@ -1,9 +1,9 @@
 package edu.cornell.gdiac.physics;
 
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -17,39 +17,24 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.ObjectSet;
 import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.assets.FilmStripLoader;
 import edu.cornell.gdiac.physics.enemy.Enemy;
+import edu.cornell.gdiac.physics.enemy.EnemyModel;
 import edu.cornell.gdiac.physics.obstacle.BoxObstacle;
+import edu.cornell.gdiac.physics.obstacle.CapsuleObstacle;
 import edu.cornell.gdiac.physics.obstacle.Obstacle;
 import edu.cornell.gdiac.physics.obstacle.PolygonObstacle;
 import edu.cornell.gdiac.physics.obstacle.WheelObstacle;
+import edu.cornell.gdiac.physics.platform.RopeBridge;
+import edu.cornell.gdiac.physics.platform.Spinner;
 import edu.cornell.gdiac.physics.player.UrsaModel;
 import edu.cornell.gdiac.physics.shadows.ShadowController;
-import edu.cornell.gdiac.physics.shadows.ShadowModel;
+//import edu.cornell.gdiac.physics.shadows.ShadowedObject;
 import edu.cornell.gdiac.physics.tree.Tree;
 import edu.cornell.gdiac.util.FilmStrip;
-import edu.cornell.gdiac.util.PooledList;
+import java.util.LinkedList;
 
 public class SceneModel extends WorldController implements ContactListener {
-
-    /*
-     * Initialize the global blob shadow used for Ursa and enemies.
-     * Since it's a shared texture, we can just use it statically across everything to make it easier.
-     */
-
-    private static final int BLOB_SHADOW_RESOLUTION = 1024; // High resolution for lesser edge cuts (in theory that is)
-
-    public static final Texture BLOB_SHADOW_TEXTURE;
-
-    static {
-        Pixmap pixmap = new Pixmap(BLOB_SHADOW_RESOLUTION * 2, BLOB_SHADOW_RESOLUTION * 2, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.BLACK);
-        int xcenter = pixmap.getWidth() / 2;
-        int ycenter = pixmap.getHeight() / 2;
-        pixmap.fillCircle(xcenter, ycenter, BLOB_SHADOW_RESOLUTION);
-        BLOB_SHADOW_TEXTURE = new Texture(pixmap);
-        pixmap.dispose();
-    }
-
     /** Texture asset for character avatar */
     private TextureRegion avatarTexture;
     private TextureRegion ursaTexture;
@@ -87,32 +72,21 @@ public class SceneModel extends WorldController implements ContactListener {
     private UrsaModel avatar;
     /** List of references to enemies */
     private Enemy[] enemies = new Enemy[20];
-
-    /**
-     * List of references to all shadows.
-     */
-    private static PooledList<ShadowModel> shadows = new PooledList<>();
-
-    /**
-     * List of all references to all trees
-     */
-    private PooledList<Tree> trees = new PooledList<>();
-
+    /** List of enemy AI controllers */
+    private LinkedList<AIController> controls = new LinkedList<>();
     private float timer = 0;
     private TextureRegion playerWalkTextureScript;
     private TextureRegion playerIdleTextureScript;
-    private TextureRegion salmonUprightWalkScript;
     private FilmStrip playerWalkFilm;
-    private FilmStrip salmonUprightWalkFilm;
     private FilmStrip playerIdleFilm;
-
+    private TextureRegion[] salmonWalk = new TextureRegion[8];
     private int playerWalkAnimIndex = 0;
     private int playerIdleAnimIndex =0;
     private int salmonWalkAnimIndex = 0;
     /** Reference to the goalDoor (for collision detection) */
     private BoxObstacle goalDoor;
     /** Controller for all dynamic shadows */
-    private ShadowController shadowController;
+    private ShadowController shadows;
 
     /** Mark set to handle more sophisticated collision callbacks */
     protected ObjectSet<Fixture> sensorFixtures;
@@ -160,9 +134,6 @@ public class SceneModel extends WorldController implements ContactListener {
         playerIdleFilm = new FilmStrip(playerIdleTextureScript.getTexture(),4,8);
         playerIdleFilm.setFrame(0);
 
-        salmonUprightWalkScript = new TextureRegion(directory.getEntry("enemies:salmonUprightWalk",Texture.class));
-        salmonUprightWalkFilm = new FilmStrip(salmonUprightWalkScript.getTexture(),3,8);
-        salmonUprightWalkFilm.setFrame(0);
 
 
 
@@ -196,11 +167,15 @@ public class SceneModel extends WorldController implements ContactListener {
         addQueue.clear();
         world.dispose();
 
+        for (AIController e : controls) e.reset();
+        controls.clear();
+
         world = new World(gravity,false);
         world.setContactListener(this);
         setComplete(false);
         setFailure(false);
         populateLevel();
+
     }
 
     /**
@@ -225,7 +200,7 @@ public class SceneModel extends WorldController implements ContactListener {
 //        addObject(goalDoor);
 
         // create shadow (idk if this does anything even)
-        shadowController = new ShadowController();
+        shadows = new ShadowController();
 
         String wname = "wall";
         JsonValue walljv = constants.get("walls");
@@ -276,7 +251,7 @@ public class SceneModel extends WorldController implements ContactListener {
         enemies[0] = new Enemy(constants.get("enemy"), dwidth, dheight);
         enemies[0].setLookDirection(1, 0);
         enemies[0].setDrawScale(scale);
-        enemies[0].setTexture(salmonUprightWalkFilm);
+        enemies[0].setTexture(enemyTexture);
         addObject(enemies[0]);
 
         dwidth  = enemyTexture2.getRegionWidth()/30;
@@ -284,13 +259,14 @@ public class SceneModel extends WorldController implements ContactListener {
         enemies[1] = new Enemy(constants.get("enemy2"), dwidth, dheight);
         enemies[1].setLookDirection(1, 0);
         enemies[1].setDrawScale(scale);
-        enemies[1].setTexture(salmonUprightWalkFilm);
+        enemies[1].setTexture(enemyTexture2);
         addObject(enemies[1]);
 
         String tname = "tree";
         JsonValue treejv = constants.get("trees");
         for(int ii = 0; ii < treejv.size; ii++) {
-            Tree obj = new Tree(treejv.get(ii).asFloatArray(),3,3);
+            Tree obj;
+            obj = new Tree(treejv.get(ii).asFloatArray(),1,3);
             obj.setBodyType(BodyDef.BodyType.StaticBody);
             obj.setDensity(defaults.getFloat( "density", 0.0f ));
             obj.setFriction(defaults.getFloat( "friction", 0.0f ));
@@ -299,9 +275,13 @@ public class SceneModel extends WorldController implements ContactListener {
             obj.setTexture(tundraTreeWithSnow);
             obj.setName(tname+ii);
             addObject(obj);
-            trees.add(obj);
-            shadows.add(new ShadowModel(new Vector2(obj.getX(), obj.getY()), Tree.X_SCALE, Tree.Y_SCALE,
-                obj.getTexture(), obj.getDrawOrigin(), obj.getDrawScale()));
+        }
+
+        // Create all AI Controllers for enemies
+        for (int i = 0; i < enemies.length; i++) {
+            if (enemies[i] != null) {
+                controls.add(new AIController(enemies[i], avatar));
+            }
         }
 
         volume = constants.getFloat("volume", 1.0f);
@@ -330,25 +310,7 @@ public class SceneModel extends WorldController implements ContactListener {
 
         return true;
     }
-    private void animateEnemies(){
-        if(salmonWalkAnimIndex == 0 || salmonWalkAnimIndex == 21){
-            salmonWalkAnimIndex = 0;
-            salmonUprightWalkFilm.setFrame(0);
-            enemies[1].setTexture(salmonUprightWalkFilm);
-            enemies[0].setTexture(salmonUprightWalkFilm);
-            salmonWalkAnimIndex +=1;
-//            System.out.println(salmonWalkAnimIndex);
-        }
-        else {
-            salmonUprightWalkFilm.setFrame(salmonWalkAnimIndex);
-            salmonWalkAnimIndex +=1;
-            enemies[1].setTexture(salmonUprightWalkFilm);
-            enemies[0].setTexture(salmonUprightWalkFilm);
-        }
-    }
-    /*
-    Animates the player model based on the conidtions of the player
-     */
+
     private void animatePlayerModel(){
         if(avatar.getXMovement() != 0 || avatar.getyMovement() != 0){
             playerIdleAnimIndex = 0;
@@ -402,56 +364,40 @@ public class SceneModel extends WorldController implements ContactListener {
         //avatar.setShooting(InputController.getInstance().didSecondary());
 
         animatePlayerModel();
-        animateEnemies();
-
-        // Shake trees
-        if (InputController.getInstance().didInteract()) {
-            Tree nearest = null;
-            float dst = 0;
-            for (Tree tree : trees) {
-                float tempDst = avatar.getPosition().dst(tree.getPosition());
-                if (tree.canShake() && tempDst < 2 && (nearest == null || tempDst < dst)) {
-                    nearest = tree;
-                    dst = tempDst;
-                }
-            }
-
-            if (nearest != null) {
-                shakeTree(nearest);
-            }
-        }
 
         avatar.applyForce();
-        enemies[0].applyForce();
-        enemies[1].applyForce();
 
         if (avatar.isJumping()) {
             jumpId = playSound( jumpSound, jumpId, volume );
         }
-        enemies[0].setAlerted(enemies[0].isPlayerInLineOfSight(world, avatar));
-        enemies[1].setAlerted(enemies[1].isPlayerInLineOfSight(world, avatar));
-        for (Enemy enemy: enemies){
-            if(enemy != null && enemy.isPlayerInLineOfSight(world,avatar)){
-                enemy.getPlayerPos(avatar.getPosition());
-            }
+//        if (avatar.isJumping()) {
+//            jumpId = playSound( jumpSound, jumpId, volume );
+//        }
 
+        // update enemies
+        for (AIController e : controls) {
+            e.getAction();
+            Enemy thisEnemy = e.getEnemy();
+
+            if (e.isWon()) setFailure(true);
+
+            thisEnemy.applyForce();
+
+            boolean inSight = thisEnemy.isPlayerInLineOfSight(world, avatar);
+            thisEnemy.setAlerted(inSight);
+            if (!inSight) e.resetLifeTime();
+
+            //manually handle collisions
+            if (Math.pow(thisEnemy.getX() - avatar.getX(),2) + Math.pow(thisEnemy.getY() - avatar.getY(), 2)
+                    <= thisEnemy.getHeight() + avatar.getHeight()) setFailure(true);
         }
+
+
 
         canvas.clear();
-        shadowController.update(this);
-    }
-
-    private void shakeTree(Tree tree) {
-        if (tree.canShake()) {
-            tree.putOnShakeCooldown();
-            System.out.println("Tree shaken");
-
-            for (Enemy enemy : enemies) {
-                if (enemy != null && enemy.getPosition().dst(tree.getPosition()) < 5) {
-                    enemy.stun();
-                }
-            }
-        }
+        canvas.begin();
+        //shadows.update();
+        canvas.end();
     }
 
     /**
@@ -535,6 +481,7 @@ public class SceneModel extends WorldController implements ContactListener {
                     (bd1 == goalDoor && bd2 == avatar)) {
                 setComplete(true);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -570,10 +517,6 @@ public class SceneModel extends WorldController implements ContactListener {
         }
     }
 
-    public static PooledList<ShadowModel> getShadows() {
-        return shadows;
-    }
-
     /** Unused ContactListener method */
     public void postSolve(Contact contact, ContactImpulse impulse) {}
     /** Unused ContactListener method */
@@ -593,6 +536,6 @@ public class SceneModel extends WorldController implements ContactListener {
 
     @Override
     public void preDraw(float dt) {
-        shadowController.drawAllShadows(canvas, this);
+        //shadows.drawAllShadows(canvas);
     }
 }
