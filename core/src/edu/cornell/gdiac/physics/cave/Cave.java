c344d890b75c758656e119e66eaaebf34a98bdc9
danbp <danbparker@yahoo.com>
2024-03-17 23:07:35 -0400

    added cave class

--- core/src/edu/cornell/gdiac/physics/cave/Cave.java
+++ core/src/edu/cornell/gdiac/physics/cave/Cave.java
@@ -1,11 +1,16 @@
 package edu.cornell.gdiac.physics.cave;
 
+import com.badlogic.gdx.graphics.Color;
+import edu.cornell.gdiac.physics.GameCanvas;
 import edu.cornell.gdiac.physics.obstacle.BoxObstacle;
 
 public class Cave extends BoxObstacle {
 
     public Cave(float width, float height) {
-        super(width, height);
+        super(5,15,width, height);
     }
 
+    public void draw(GameCanvas canvas) {
+        canvas.draw(texture,Color.WHITE,origin.x,origin.y,getX()*drawScale.x,getY()*drawScale.y,getAngle(),.05f,0.05f);
+    }
 }
