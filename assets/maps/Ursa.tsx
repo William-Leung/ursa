32f49af9f7c2bf217f9ce67e8cf67421064c87be
Dan <danbparker@yahoo.com>
2024-03-29 13:54:41 -0400

    working on tiled

---
+++ assets/maps/Ursa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Ursa" tilewidth="512" tileheight="512" tilecount="6" columns="3">
+ <image source="../player/Ursa.png" width="1668" height="1388"/>
+</tileset>
