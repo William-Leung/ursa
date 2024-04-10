2be5e0e319158777110b337b1d70aa265df35cb8
yk647 <yk647@cornell.edu>
2024-04-09 20:20:55 -0400

    Work on adding varied obstacles
    - Textures and shadows uploaded
    - House class and code (temporarily hidden due to Tiled related merge conflict)
    - Asset references updated

---
+++ core/src/edu/cornell/gdiac/physics/gameobjects/House.java
@@ -0,0 +1,12 @@
+package edu.cornell.gdiac.physics.gameobjects;
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import edu.cornell.gdiac.physics.obstacle.BoxObstacle;
+import edu.cornell.gdiac.physics.obstacle.PolygonObstacle;
+
+public class House extends PolygonObstacle {
+    public House(float[] points, float x, float y) {
+        super(points, x, y);
+        setBodyType(BodyDef.BodyType.StaticBody);
+    }
+}
