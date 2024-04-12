cda4b7b933ae724ec23ecf449a57e1199940b435
danbp <danbparker@yahoo.com>
2024-04-12 19:53:37 -0400

    added markers for enemy positions

---
+++ assets/maps/marker.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="marker" tilewidth="126" tileheight="126" tilecount="1" columns="1">
+ <image source="../tiles/marker.png" width="126" height="126"/>
+</tileset>
