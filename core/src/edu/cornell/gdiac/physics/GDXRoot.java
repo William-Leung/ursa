3435781b3ac466cbacd69d43b38d47a3ae46adfc
Dan <danbparker@yahoo.com>
2024-04-18 16:49:27 -0400

    updated level selector

--- core/src/edu/cornell/gdiac/physics/GDXRoot.java
+++ core/src/edu/cornell/gdiac/physics/GDXRoot.java
@@ -139,8 +139,8 @@ public class GDXRoot extends Game implements ScreenListener {
 			loading.dispose();
 			loading = null;
 		} else if (screen == levelSelector && exitCode == 1) {
-			controllers[current].dispose();
 			current = 0;
+			controllers[current].dispose();
 			controllers[current] = new SceneModel("level4.json");
 			controllers[current].gatherAssets(directory);
 			controllers[current].setScreenListener(this);
