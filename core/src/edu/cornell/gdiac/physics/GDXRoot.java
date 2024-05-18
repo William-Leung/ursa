8ea5962bd3ed326dc22d524c1bdcd0fcdae90f14
Lin <zekailin522@gmail.com>
2024-05-18 13:05:32 -0400

    Update GDXRoot.java

--- core/src/edu/cornell/gdiac/physics/GDXRoot.java
+++ core/src/edu/cornell/gdiac/physics/GDXRoot.java
@@ -79,12 +79,12 @@ public class GDXRoot extends Game implements ScreenListener {
 		controllers[1] = new SceneModel("rigel_tutorial_2_Ver_Z.json");
 		controllers[2] = new SceneModel("rigel_tutorial_2.5_Ver_Z.json");
 		controllers[3] = new SceneModel("rigel_tutorial_3_Ver_Z.json");
-		controllers[4] = new SceneModel("rigel_tutorial_4_Ver_Z.json");
-		controllers[5] = new SceneModel("rigel_tutorial_5_Ver_W.json");
-		controllers[6] = new SceneModel("rigel_tutorial_6_Ver_Z.json");
-		controllers[7] = new SceneModel("rigel_tutorial_7_Ver_Z.json");
+		controllers[4] = new SceneModel("rigel_tutorial_5_Ver_Z.json");
+		controllers[5] = new SceneModel("rigel_tutorial_6_Ver_W.json");
+		controllers[6] = new SceneModel("rigel_tutorial_7_Ver_Z.json");
+		controllers[7] = new SceneModel("dylan_level_1_Ver_A.json");
 		controllers[8] = new SceneModel("athena_level_Ver_A.json");
-		controllers[9] = new SceneModel("dylan_level_1_Ver_A.json");
+		controllers[9] = new SceneModel("rigel_tutorial_4_Ver_Z.json");
 		controllers[10] = new SceneModel("danny_level_Ver_Z.json");
 		controllers[11] = new SceneModel("dylan_level_2_Ver_Z.json");
 		controllers[12] = new SceneModel("zekai_level_complete.json");
